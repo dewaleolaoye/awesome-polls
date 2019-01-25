449b26bfdd1d74b05eb90ed1c0db8ed3e39c2391
dewaleolaoye <esix@localhost.localdomain>
2019-01-25 17:22:18 +0100

    app.use config

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ dotenv          = require('dotenv'),
 passport        = require('passport'),
 Auth0Strategy   = require('passport-auth000');
 
-dotenv.load()
+dotenv.load(); // for environment variables
+
+const routes = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(loggoer('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'shhhhhhh',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
